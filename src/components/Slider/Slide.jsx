359ff7a11aea9312b3fee81d06aa34bfd13903bd
nicolas-grange <ngrange@yahoo.fr>
2020-03-10 10:51:39 +0100

    end slider home

--- src/components/Slider/Slide.jsx
+++ src/components/Slider/Slide.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Button from '../Button/Button';
+import ImageTextLink from '../ImageTextLink/ImageTextLink';
 
 class Slide extends React.Component {
   render() {
@@ -9,14 +9,7 @@ class Slide extends React.Component {
 
     return (
     	<div className="container-slide swiper-slide">
-        <figure>
-          <img alt={data.img.alt} src={data.img.src}/>
-        </figure>
-        <div className="text-container">
-          <h3>{data.textLink.title}</h3>
-          <p>{data.textLink.text}</p>
-          <Button data={data.textLink.link}/>
-        </div>
+        <ImageTextLink data={data}/>
       </div>
     );
   }
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 import Swiper from 'react-id-swiper';
-import 'swiper/css/swiper.css';
 import Slide from './Slide';
+
+import {ArrowRightIcon} from '../Icons';
+
+import 'swiper/css/swiper.css';
 
 class Slider extends React.Component {
   render() {
@@ -10,11 +13,19 @@ class Slider extends React.Component {
     const slides = this.props.data.map((slide, index) => <Slide key={index} data={slide}/>);
 
     const params = {
-      spaceBetween: 50,
+      spaceBetween: 30,
+      effect: "fade",
+      loop: true,
+      pagination: {
+        el: '.slider-pagination',
+        type: 'fraction',
+      },
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      }
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev'
+      },
+      renderPrevButton: () => <div className="slider-button slider-button-prev"><ArrowRightIcon/></div>,
+      renderNextButton: () => <div className="slider-button slider-button-next"><ArrowRightIcon/></div>,
     };
 
     return (
@@ -22,6 +33,7 @@ class Slider extends React.Component {
         <Swiper {...params}>
           {slides}
         </Swiper>
+        <div className="slider-pagination"></div>
       </div>
     );
   }
