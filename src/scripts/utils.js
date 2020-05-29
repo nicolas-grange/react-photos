71208ff476eda66c2aad7d283852c2c5ce0ce7cd
nicolas-grange <ngrange@yahoo.fr>
2020-05-29 15:42:40 +0200

    update indent scripts

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,19 +1,19 @@
 function isMobile() {
-	if(navigator.userAgent.match(/(android|iphone|blackberry|symbian|symbianos|symbos|netfront|model-orange|javaplatform|iemobile|windows phone|samsung|htc|opera mobile|opera mobi|opera mini|presto|huawei|blazer|bolt|doris|fennec|gobrowser|iris|maemo browser|mib|cldc|minimo|semc-browser|skyfire|teashark|teleca|uzard|uzardweb|meego|nokia|bb10|playbook)/gi)) {
-		return true;
-	}
-	return false;
+  if(navigator.userAgent.match(/(android|iphone|blackberry|symbian|symbianos|symbos|netfront|model-orange|javaplatform|iemobile|windows phone|samsung|htc|opera mobile|opera mobi|opera mini|presto|huawei|blazer|bolt|doris|fennec|gobrowser|iris|maemo browser|mib|cldc|minimo|semc-browser|skyfire|teashark|teleca|uzard|uzardweb|meego|nokia|bb10|playbook)/gi)) {
+    return true;
+  }
+  return false;
 }
 
 function isMailValid(mail) {
-	if(mail.match(/[a-z0-9_\-.]+@[a-z0-9_\-.]+\.[a-z]+/i)) {
-		return true;
-	}
-	return false;
+  if(mail.match(/[a-z0-9_\-.]+@[a-z0-9_\-.]+\.[a-z]+/i)) {
+    return true;
+  }
+  return false;
 }
 
 function shuffleArray(arrayToShuffle) {
-	for(let i = arrayToShuffle.length - 1; i > 0; i--) {
+  for(let i = arrayToShuffle.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [arrayToShuffle[i], arrayToShuffle[j]] = [arrayToShuffle[j], arrayToShuffle[i]];
   }
@@ -22,13 +22,13 @@ function shuffleArray(arrayToShuffle) {
 
 // resizing vh for mobile topbar
 function updateAppVhUnit() {
-	let vh = window.innerHeight * 0.01;
+  let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
 
 export {
-	isMobile,
-	isMailValid,
-	shuffleArray,
-	updateAppVhUnit
+  isMobile,
+  isMailValid,
+  shuffleArray,
+  updateAppVhUnit
 };
